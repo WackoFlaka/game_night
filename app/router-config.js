256483affc8715859ee3834bfff1235188f98581
Joaquin Chavez <jchav5601@gmail.com>
2024-02-09 08:56:44 -0700

    finished game night

--- app/router-config.js
+++ app/router-config.js
@@ -6,15 +6,8 @@ import { Router } from "./utils/Router.js";
 export const router = new Router([
   {
     path: '',
-    controllers: [HomeController, ExamplesController],
-    view: /*html*/`
-    <div class="bg-white p-3">
-      <div class="card-body">
-        <p>Home Page</p>
-        <button class="btn btn-dark" onclick="app.HomeController.testButton()">😎</button>
-      </div>
-    </div>
-    `
+    controllers: [playerController],
+    view: ''
   },
   {
     path: '#/about',
